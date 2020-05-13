229aaf9c717e8ed0b777ad41bb7ffd6d92be64ff
dengqiming <dengqiming@izaiqi.com>
2020-05-13 13:44:59 +0800

    feat: 修改依赖关系

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* 
+/*
 * @Author:             Roy
 * @Date:               2015-12-24 18:04:33
 * @Description:        For font Subset
@@ -13,39 +13,40 @@ var mime        = require("mime");
 var fontMinTool = require('./tools/fontMinTool.js');
 var Extension = require('./tools/extension.js');
 var FontSubset = function(source){
-	
+
 	var callback = this.async();		//转为异步执行
 	this.cacheable && this.cacheable();
-	
+
 	if(!this.emitFile) throw new Error("emitFile is required from module system");
-	
-	var query = loaderUtils.parseQuery(this.query);
-	query.ext = Extension.getExtension(this.resourcePath);
+
+	var options = loaderUtils.getOptions(this);
+
+  options.ext = Extension.getExtension(this.resourcePath);
 
 	var getFontSubset = new fontMinTool({			//创建取子集任务
 		srcPath:this.resourcePath,
-		text:query.text,
-		ext:query.ext
+		text:options.text,
+		ext:options.ext
 	})
 	getFontSubset.run((err, files, stream) => {
 		if (err) return console.log(err);		// 任务执行异常捕获
-					
+
 		var FontSubsetContent = files[1].contents;
 		var size              = FontSubsetContent.length;
-		var limit             = query.limit || 0;
+		var limit             = options.limit || 0;
 		var cbParam           = '';
 
 		if (limit && size > limit) {		//小于limit时转为BASE64
 
-			var mimetype = query.mimetype || mime.lookup(this.resourcePath);	
+			var mimetype = options.mimetype || mime.lookup(this.resourcePath);
 			cbParam =  "module.exports = " + JSON.stringify("data:" + (mimetype ? mimetype + ";" : "") + "base64," + FontSubsetContent.toString("base64"));
-		
+
 		}else{		//生成指定字体文件
 
-			var url = loaderUtils.interpolateName(this, query.name || '[name].[ext]', {
-				context: query.context || this.options.context,
+			var url = loaderUtils.interpolateName(this, options.name || '[name].[ext]', {
+				context: options.context || this.options.context,
 				content: FontSubsetContent,
-				regExp: query.regExp
+				regExp: options.regExp
 			});
 			this.emitFile(url, FontSubsetContent);
 			cbParam = "module.exports = __webpack_public_path__ + " + JSON.stringify(url) + ";";
@@ -53,7 +54,7 @@ var FontSubset = function(source){
 
 		callback(null,cbParam);
 	});
-	
+
 }
 
 module.exports = FontSubset;
